9225ac8b85d52ca244631f23d2c3334833523d62
sam <sammas20040826@gmail.com>
2021-12-13 11:11:38 +0800

    2

--- test.py
+++ test.py
@@ -29,34 +29,34 @@ if __name__ == '__main__':
  
 def detectImageUrl(self,imageurl):
  
-headers = {
-    # Request headers
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': self.readConfig()['api_key'],
-}
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': self.readConfig()['api_key'],
+    }
  
-params = urllib.parse.urlencode({
-    # Request parameters
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender',
-    #'recognitionModel': 'recognition_04',
-    'returnRecognitionModel': 'false',
-    'detectionModel': 'detection_01',
-    'faceIdTimeToLive': '86400',
-})
-print('imageurl=',imageurl)
-requestbody = '{"url":"'+ imageurl+' ''}'
-try:
-    conn = http.client.HTTPSConnection( self.readConfig()['host'])
-    conn.request("POST", "/face/v1.0/detect?%s" % params, requestbody, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
- 
-    print("detectLocalImage:",f"{imageurl} 偵測到 {len(json_face_detect)} 個人")
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender',
+        #'recognitionModel': 'recognition_04',
+        'returnRecognitionModel': 'false',
+        'detectionModel': 'detection_01',
+        'faceIdTimeToLive': '86400',
+    })
+    print('imageurl=',imageurl)
+    requestbody = '{"url":"'+ imageurl+' ''}'
+    try:
+        conn = http.client.HTTPSConnection( self.readConfig()['host'])
+        conn.request("POST", "/face/v1.0/detect?%s" % params, requestbody, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    
+        print("detectLocalImage:",f"{imageurl} 偵測到 {len(json_face_detect)} 個人")
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
  
  
